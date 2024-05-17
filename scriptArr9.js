ac9c09dafe851e60ce3b04706a2777ebd362b2a3
Farewell21 <114603939+Farewell21@users.noreply.github.com>
2024-05-17 14:44:24 +0800

    Add files via upload

---
+++ scriptArr9.js
@@ -0,0 +1,10 @@
+axios.get('http://localhost:5000/employees')
+    .then(response => {
+        const data = response.data[9]; 
+        document.getElementById('EmployeeFullName').innerText = `Driver\n${data.employeeFullName}`;
+        document.getElementById('employeeAddress').innerText = data.employeeAddress;
+        document.getElementById('employeeEmail').innerText = data.employeeEmail;
+    })
+    .catch(error => {
+        console.error('Error fetching employee data:', error);
+    });
